f68c33a9215a2d13f2285a31b4d65a5b0cacb920
½ÒúÖñ <906656782@qq.com>
2021-08-13 12:09:21 +0800

    add list comp

--- DataStruct/DataStruct/List.h
+++ DataStruct/DataStruct/List.h
@@ -3,6 +3,189 @@
 
 #include"Common.h"
 
+typedef struct ListNode
+{
+	ElemType data;
+	struct ListNode *next;
+}ListNode;
+
+typedef ListNode* List;
+
+//函数的申明
+ListNode* _Buynode(ElemType v);
+void ListInit(List *plist);
+void ListShow(List plist);
+void ListPushBack(List *plist, ElemType v);
+void ListPushFront(List *plist, ElemType v);
+void ListPopBack(List *plist);
+void ListPopFront(List *plist);
+
+void ListErase(List *plist, ElemType key);
+void ListClear(List *plist);
+void ListDestroy(List *plist);
+
+ListNode* ListFind(List plist, ElemType key);
+size_t ListLength(List plist);
+
+//函数的实现
+
+ListNode* _Buynode(ElemType v)
+{
+	ListNode *s = (ListNode*)malloc(sizeof(ListNode));
+	assert(s != NULL);
+	s->data = v;
+	s->next = NULL;
+
+	return s;
+}
+
+void ListInit(List *plist)
+{
+	*plist = NULL;
+}
+
+void ListPushBack(List *plist, ElemType v)
+{
+	//申请节点
+	ListNode *s = _Buynode(v);
+
+	//插入节点
+	ListNode *p = *plist;
+	if(p == NULL)
+	{
+		*plist = s;
+		return;
+	}
+
+	while(p->next != NULL)
+		p = p->next;
+
+	p->next = s;
+}
+
+void ListPushFront(List *plist, ElemType v)
+{
+	ListNode *s = _Buynode(v);
+	s->next = *plist;
+	*plist = s;
+}
+
+void ListPopBack(List *plist)
+{
+	ListNode *p = *plist;
+	ListNode *prev = NULL;
+
+	if(*plist == NULL)
+		return;
+	
+	while(p->next != NULL)
+	{
+		prev = p;
+		p = p->next;
+	}
+
+	if(prev == NULL)
+		*plist = NULL;
+	else
+		prev->next = NULL;
+	free(p);
+}
+
+void ListPopFront(List *plist)
+{
+	ListNode *p = *plist;
+	if(*plist == NULL)
+		return;
+
+	*plist = p->next;
+	free(p);
+}
+
+void ListShow(List plist)
+{
+	ListNode *p = plist;
+	while(p != NULL)
+	{
+		printf("%d->", p->data);
+		p = p->next;
+	}
+	printf("Over.\n");
+}
+
+size_t ListLength(List plist)
+{
+	ListNode *p = plist;
+	size_t len = 0;
+	while(p != NULL)
+	{
+		len++;
+		p = p->next;
+	}
+	return len;
+}
+
+ListNode* ListFind(List plist, ElemType key)
+{
+	ListNode *p = plist;
+	while(p!=NULL && p->data!=key)
+		p = p->next;
+	return p;
+
+	/*
+	ListNode *p = plist;
+	while(p != NULL)
+	{
+		if(p->data == key)
+			return p;
+		p = p->next;
+	}
+	return NULL;
+	*/
+}
+
+void ListErase(List *plist, ElemType key)
+{
+	ListNode *p = *plist;
+	ListNode *prev = NULL;
+	if(p == NULL)
+		return;
+
+	while(p!=NULL && p->data!=key)
+	{
+		prev = p;
+		p = p->next;
+	}
+	if(p == NULL)
+		return;
+
+	if(prev == NULL)
+		*plist = p->next;
+	else
+		prev->next = p->next;
+	free(p);
+}
+
+void ListClear(List *plist)
+{
+	ListNode *p = NULL;
+	while(*plist != NULL)
+	{
+		p = *plist;
+		*plist = p->next;
+		free(p);
+	}
+}
+
+void ListDestroy(List *plist)
+{
+	ListClear(plist);
+}
+
+
+
+/////////////////////////////////////////////////////////////////////
+
+/*
 //定义节点类型
 typedef struct ListNode
 {
@@ -12,13 +195,84 @@ typedef struct ListNode
 
 typedef ListNode* List;
 
+//不带头结点
 void InitList(List *plist)
 {
 	*plist = NULL;
 }
 
+void CreateList_Tail(List *plist)
+{
+	ListNode *p = *plist;
+	for(int i=1; i<=10; ++i)
+	{
+		ListNode *s = (ListNode*)malloc(sizeof(ListNode));
+		assert(s != NULL);
+		s->data = i;
+		s->next = NULL;
+
+		if(p == NULL)
+			*plist = s;
+		else
+			p->next = s;
+		p = s;
+	}
+}
+void ShowList(List plist)
+{
+	ListNode *p = plist;
+	while(p != NULL)
+	{
+		printf("%d->", p->data);
+		p = p->next;
+	}
+	printf("Over.\n");
+}
+
+/*
+//带头结点
+void InitList(List *plist)
+{
+	//申请头结点
+	*plist = (ListNode*)malloc(sizeof(ListNode));
+	(*plist)->next = NULL;
+}
+
+void CreateList_Tail(List *plist)
+{
+	ListNode *p = *plist;
+	for(int i=1; i<=10; ++i)
+	{
+		ListNode *s = (ListNode*)malloc(sizeof(ListNode));
+		assert(s != NULL);
+		s->data = i;
+		s->next = NULL;
+
+		p->next = s;
+		p = s;
+	}
+}
+void ShowList(List plist)
+{
+	ListNode *p = plist->next;
+	while(p != NULL)
+	{
+		printf("%d->", p->data);
+		p = p->next;
+	}
+	printf("Over.\n");
+}
+
+/*
+//不带头结点
+void InitList(List *plist)
+{
+	*plist = NULL;
+}
+
+
 //1->2->3->4->5->....->10
-void CreateList(List *plist)
+void CreateList_Tail(List *plist)
 {
 	*plist = (ListNode*)malloc(sizeof(ListNode));
 	assert(*plist != NULL);
@@ -39,6 +293,24 @@ void CreateList(List *plist)
 	}
 }
 
+void CreateList_Front(List *plist)
+{
+	*plist = (ListNode*)malloc(sizeof(ListNode));
+	assert(*plist != NULL);
+	(*plist)->data = 1;
+	(*plist)->next = NULL;
+
+	//头插法
+	for(int i=2; i<=10; ++i)
+	{
+		ListNode *s = (ListNode*)malloc(sizeof(ListNode));
+		assert(s != NULL);
+		s->data = i;
+		s->next = *plist;
+		*plist = s;
+	}
+}
+
 void ShowList(List plist)
 {
 	ListNode *p = plist;
@@ -49,5 +321,5 @@ void ShowList(List plist)
 	}
 	printf("Over.\n");
 }
-
+*/
 #endif /* _LIST_H_ */
--- DataStruct/DataStruct/TestMain.c
+++ DataStruct/DataStruct/TestMain.c
@@ -1,21 +1,16 @@
 #include"SeqList.h"
 #include"List.h"
 
-void main()
-{
-	List mylist;//  ListNode *mylist;
-	InitList(&mylist);
-	CreateList(&mylist);
-	ShowList(mylist);
-}
-
-/*
 int main(int argc, char *argv[])
 {
-	SeqList mylist;
-	SeqListInit(&mylist, 8);
+	//SeqList mylist;
+	//SeqListInit(&mylist, 8);
+
+	List mylist;
+	ListInit(&mylist);
 
 	ElemType item, value, key;
+	ListNode *p = NULL;
 	int pos;
 	int select = 1;
 	while(select)
@@ -24,11 +19,11 @@ int main(int argc, char *argv[])
 		printf("* [1] push_back           [2] push_front   *\n");
 		printf("* [3] show_list           [0] quit_system  *\n");
 		printf("* [4] pop_back            [5] pop_front    *\n");
-		printf("* [6] insert_pos          [7] insert_val   *\n");
-		printf("* [8] erase_pos           [9] erase_val    *\n");
-		printf("* [10] find_pos           [11] find_val    *\n");
+		printf("* [*6] insert_pos         [7] insert_val   *\n");
+		printf("* [*8] erase_pos          [9] erase_val    *\n");
+		printf("* [*10] find_pos          [11] find_val    *\n");
 		printf("* [12] sort               [13] reverse     *\n");
-		printf("* [14] length             [15] capacity    *\n");
+		printf("* [14] length             [*15] capacity   *\n");
 		printf("* [16] clear              [*17] destroy    *\n");
 		printf("********************************************\n");
 		printf("请选择:>");
@@ -42,84 +37,83 @@ int main(int argc, char *argv[])
 			printf("请输入要插入的数据<以-1结束>:>");
 			while(scanf("%d", &item), item!=-1)
 			{
-				SeqListPushBack(&mylist, item);
+				ListPushBack(&mylist, item);
 			}
 			break;
 		case 2:
 			printf("请输入要插入的数据<以-1结束>:>");
 			while(scanf("%d", &item), item!=-1)
 			{
-				SeqListPushFront(&mylist, item);
+				ListPushFront(&mylist, item);
 			}
 			break;
 		case 3:
-			SeqListShow(&mylist);
+			ListShow(mylist);
 			break;
 		case 4:
-			SeqListPopBack(&mylist);
+			ListPopBack(&mylist);
 			break;
 		case 5:
-			SeqListPopFront(&mylist);
+			ListPopFront(&mylist);
 			break;
 		case 6:
 			printf("请输入要插入的位置:>");
 			scanf("%d", &pos);
 			printf("请输入要插入的值:>");
 			scanf("%d", &item);
-			SeqListInsertByPos(&mylist, pos, item);
+			//SeqListInsertByPos(&mylist, pos, item);
 			break;
 		case 7:
 			printf("请输入要插入的值:>");
 			scanf("%d", &item);
-			SeqListInsertByVal(&mylist, item);
+			//SeqListInsertByVal(&mylist, item);
 			break;
 		case 8:
 			printf("请输入要删除的位置:>");
 			scanf("%d", &pos);
-			SeqListEraseByPos(&mylist, pos);
+			//SeqListEraseByPos(&mylist, pos);
 			break;
 		case 9:
 			printf("请输入要删除的值:>");
 			scanf("%d", &key);
-			SeqListEraseByVal(&mylist, key);
+			ListErase(&mylist, key);
 			break;
 		case 10:
 			printf("请输入要查找的位置:>");
 			scanf("%d", &pos);
-			value = SeqListFindByPos(&mylist, pos);
+			//value = SeqListFindByPos(&mylist, pos);
 			printf("在%d位置的值为:> %d\n", pos, value);
 			break;
 		case 11:
 			printf("请输入要查找的值:>");
 			scanf("%d", &key);
-			pos = SeqListFindByVal(&mylist, key);
-			if(pos == -1)
+			p = ListFind(mylist, key);
+			if(p == NULL)
 				printf("要查找的%d数据不存在.\n", key);
 			else
-				printf("要查找的数据%d在 %d位置.\n", key, pos);
+				printf("%d数据已被查找到.\n", key);
 			break;
 		case 12:
-			SeqListSort(&mylist);
+			//SeqListSort(&mylist);
 			break;
 		case 13:
-			SeqListReverse(&mylist);
+			//SeqListReverse(&mylist);
 			break;
 		case 14:
-			printf("顺序表的长度为:> %d\n", SeqListLength(&mylist));
+			printf("顺序表的长度为:> %d\n", ListLength(mylist));
 			break;
 		case 15:
-			printf("顺序表的容量为:> %d\n", SeqListCapacity(&mylist));
+			//printf("顺序表的容量为:> %d\n", SeqListCapacity(&mylist));
 			break;
 		case 16:
-			SeqListClear(&mylist);
+			ListClear(&mylist);
 			break;
 		default:
 			break;
 		}
 	}
 
-	SeqListDestroy(&mylist);
+	ListDestroy(&mylist);
 	printf("GoodBye.\n");
 	return 0;
 }
-*/
